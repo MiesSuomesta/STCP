0a1da376a196b4489761914620ab7fdb2c04cee8
Lauri Jakku <lja@lja.fi>
2025-02-22 08:22:08 +0200

    the pristine build of Cython to binaries, needs work

---
+++ ports/Cython/test_client_main.c
@@ -0,0 +1,30 @@
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+int main(int argc, char *argv[]) {
+    Py_Initialize();
+
+    // Lisää hakupolku, jotta Python löytää moduulin
+    PyRun_SimpleString("import sys\nsys.path.insert(0, '.')");
+
+    // Lataa `test_client`-moduuli
+    PyObject *pModule = PyImport_ImportModule("test_client");
+    if (!pModule) {
+        PyErr_Print();
+        return 1;
+    }
+
+    // Hae `main()`-funktio ja suorita se
+    PyObject *pFunc = PyObject_GetAttrString(pModule, "main");
+    if (pFunc && PyCallable_Check(pFunc)) {
+        PyObject_CallObject(pFunc, NULL);
+    } else {
+        PyErr_Print();
+    }
+
+    Py_XDECREF(pFunc);
+    Py_XDECREF(pModule);
+    Py_Finalize();
+
+    return 0;
+}

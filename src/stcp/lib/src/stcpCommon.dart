314f42b4686a4ef5ef3283b85c8d433c762327b3
Lauri Jakku <lja@lja.fi>
2025-03-15 12:34:05 +0200

    STCP-protocol version 3.0 (dart)
      * Desingned as classic connection, improved
        how protocol negotiate connection + how
        user can receive and send data over STCP-
        connection.
    
      * This version is better integrateble for
        any application.
    
      * User can receive messages via callback or
        recv/send pattern.

--- src/stcp/lib/src/stcpCommon.dart
+++ src/stcp/lib/src/stcpCommon.dart
@@ -12,6 +12,7 @@ import 'dart:convert';
 
 import 'package:paxlog/paxlog.dart' as logi;
 import 'package:the_secure_comm/the_secure_comm.dart' as scom;
+import 'package:utils/utils.dart' as utils;
 
 /*
    IV Size: 16, 24 or 32 bytes. default: 32
@@ -19,14 +20,14 @@ import 'package:the_secure_comm/the_secure_comm.dart' as scom;
 const int STCP_AES_KEY_SIZE_IN_BYTES = 32;
 const int STCP_AES_IV_SIZE_IN_BYTES = 16;
 
-typedef processMsgFunction =
-    Uint8List? Function(Socket theSock, Uint8List msgIn);
+typedef processMsgFunction = Future<Uint8List?> Function(
+    Socket theSock, Uint8List msgIn);
 typedef theHandshakeFunction = void Function(Socket theSock);
 
-typedef theHandshakeAESGotFunction =
-    Future<void> Function(Socket theSocket, Uint8List theAesKeyGot);
-typedef theHandshakePublicKeyGotFunction =
-    Uint8List Function(Socket theSocket, Uint8List thePublicKey);
+typedef theHandshakeAESGotFunction = Future<void> Function(
+    Socket theSocket, Uint8List theAesKeyGot);
+typedef theHandshakePublicKeyGotFunction = Uint8List Function(
+    Socket theSocket, Uint8List thePublicKey);
 
 Uint8List bigIntToBytes(BigInt value) {
   var hexString = value.toRadixString(16);
@@ -60,7 +61,7 @@ class EllipticCodec {
     keys = generateKeyPair();
   }
 
-  String deriveSharedKeyBasedAESKey(Uint8List paramSharedSecret) {
+  Uint8List deriveSharedKeyBasedAESKey(Uint8List paramSharedSecret) {
     // Käytä HMAC-avain
     var hmac = crypto.Hmac(crypto.sha256, paramSharedSecret);
 
@@ -68,7 +69,10 @@ class EllipticCodec {
       Uint8List(STCP_AES_KEY_SIZE_IN_BYTES),
     );
     String out = String.fromCharCodes(aesKeyList.bytes);
-    return base64Encode(out.codeUnits).substring(0, STCP_AES_KEY_SIZE_IN_BYTES);
+    String theRV =
+        base64Encode(out.codeUnits).substring(0, STCP_AES_KEY_SIZE_IN_BYTES);
+    Uint8List rv = Uint8List.fromList(theRV.codeUnits);
+    return rv;
   }
 
   Uint8List signMessage(String message, ECPrivateKey privateKey) {
@@ -130,7 +134,7 @@ class EllipticCodec {
       org = publicKeyBytesWithType;
     }
 
-    logi.logAndGetStringFromBytes("publicKeyToBytes", org);
+    utils.logAndGetStringFromBytes("publicKeyToBytes", org);
     return org;
   }
 
@@ -156,6 +160,9 @@ class EllipticCodec {
 
   ECPublicKey bytesToPublicKey(Uint8List publicKeyBytes) {
     // Varmista, että julkinen avain alkaa oikealla tavulla
+
+    logi.traceIntList("Public key raw data in", publicKeyBytes);
+
     if (publicKeyBytes[0] != 0x04) {
       throw ArgumentError("Invalid public key encoding.");
     }
@@ -178,54 +185,64 @@ class EllipticCodec {
 
   Uint8List computeSharedSecretAsBytes(PublicKey paramPublicKey) {
     BigInt agreement = computeSharedSecret(paramPublicKey);
-    return bigIntToBytes(agreement);
+    Uint8List rv = bigIntToBytes(agreement);
+    logi.traceIntList("Return value", rv);
+    return rv;
   }
 }
 
 class StcpCommon {
   Uint8List theSecureMessageTransferIncoming(
     Uint8List msgIncomingCrypted,
-    String theAESpresharedkey,
+    Uint8List theAESpresharedkeyList,
   ) {
-    logi.logAndGetStringFromBytes(
-      "theSecureMessageTransferIncoming raw",
-      msgIncomingCrypted,
-    );
+    logi.traceIntList("MSG TRANSFER incoming", theAESpresharedkeyList);
+    String theAESpresharedkey =
+        String.fromCharCodes(theAESpresharedkeyList.toList());
 
     String theAesKey = theAESpresharedkey
         .padRight(STCP_AES_KEY_SIZE_IN_BYTES, "0")
         .substring(0, STCP_AES_KEY_SIZE_IN_BYTES);
 
     logi.traceMe("The AES key in incoming: $theAesKey");
-    String strIncomingPacket = logi.logAndGetStringFromBytes(
+    String strIncomingPacket = utils.logAndGetStringFromBytes(
       "the IV vector + Payload of message in",
       msgIncomingCrypted,
     );
 
-    Uint8List theIncomingIV = logi.logAndGetBytesFromString(
+    Uint8List theIncomingIV = utils.logAndGetBytesFromString(
       "the incoming IV-vector",
       strIncomingPacket.substring(0, STCP_AES_IV_SIZE_IN_BYTES),
     );
+    logi.traceIntList("MSG TRANSFER incoming IV", theIncomingIV);
 
-    Uint8List theEncryptedMessage = logi.logAndGetBytesFromString(
+    Uint8List theEncryptedMessage = utils.logAndGetBytesFromString(
       "the Encrypted message",
       strIncomingPacket.substring(STCP_AES_IV_SIZE_IN_BYTES),
     );
 
+    logi.traceIntList("MSG TRANSFER incoming enc MSG", theEncryptedMessage);
+
+    String theIV = String.fromCharCodes(theIncomingIV.toList());
+
     scom.SecureCommunication theSCincomign = scom.SecureCommunication(
       theAesKey,
-      theIncomingIV,
-      name: "Incoming AES traffic",
+      theIV,
+      "Incoming AES traffic",
     );
 
-    logi.logAndGetBytesFromString("theAesKey", theAesKey);
-    logi.logAndGetStringFromBytes("theIncomingIV", theIncomingIV.toList());
-    logi.logAndGetStringFromBytes(
-      "encrypted msg",
-      theEncryptedMessage.toList(),
-    );
-    String decryptedMessage = theSCincomign.decrypt(theEncryptedMessage);
-    Uint8List tmp = logi.logAndGetBytesFromString(
+    utils.logAndGetBytesFromString("theAesKey", theAesKey);
+    utils.logAndGetStringFromBytes("theIncomingIV", theIncomingIV);
+    logi.traceIntList("the Encrypted message", theEncryptedMessage);
+
+    String theEncryptedMessageStr =
+        String.fromCharCodes(theEncryptedMessage.toList());
+    String decryptedMessage = theSCincomign.decrypt(theEncryptedMessageStr);
+
+    logi.traceIntList("MSG TRANSFER incoming decrypt",
+        Uint8List.fromList(decryptedMessage.codeUnits));
+
+    Uint8List tmp = utils.logAndGetBytesFromString(
       "Decrypted message",
       decryptedMessage,
     );
@@ -234,43 +251,60 @@ class StcpCommon {
 
   Uint8List theSecureMessageTransferOutgoing(
     Uint8List msgOutgoingPlain,
-    String theAESpresharedkey,
+    Uint8List theAESpresharedkey,
   ) {
-    String theAesKey = theAESpresharedkey
+    logi.traceIntList("MSG TRANSFER incoming", theAESpresharedkey);
+    String theAESpresharedkeyStr =
+        String.fromCharCodes(theAESpresharedkey.toList());
+
+    String theAesKey = theAESpresharedkeyStr
         .padRight(STCP_AES_KEY_SIZE_IN_BYTES, "0")
         .substring(0, STCP_AES_KEY_SIZE_IN_BYTES);
+    Uint8List theAesKeyUintList = Uint8List.fromList(theAesKey.codeUnits);
+
+    logi.traceMe("The AES key in incoming: $theAesKey");
 
     Uint8List theOutgoingIV = generateRandomBase64(STCP_AES_IV_SIZE_IN_BYTES);
 
-    logi.traceMe("MSG OUT KEY: ${theAesKey.length} // $theAesKey //");
+    logi.traceMeBetter("MSG OUT KEY: ${theAesKey.length} // $theAesKey //");
     logi.traceMe("MSG OUT IV : ${theOutgoingIV.length} // $theOutgoingIV //");
-    logi.logAndGetStringFromBytes("MSG OUT raw plain", msgOutgoingPlain);
+    utils.logAndGetStringFromBytes("MSG OUT raw plain", msgOutgoingPlain);
+
+    String theOutgoingIVStr = String.fromCharCodes(theOutgoingIV.toList());
 
     scom.SecureCommunication theSC = scom.SecureCommunication(
       theAesKey,
-      theOutgoingIV,
-      name: "Outgoing msg",
+      theOutgoingIVStr,
+      "Outgoing msg",
     );
 
-    String outgoingIVvector = logi.logAndGetStringFromBytes(
+    String outgoingIVvector = utils.logAndGetStringFromBytes(
       "MSG OUT raw IV Vector",
-      theOutgoingIV.toList(),
+      theOutgoingIV,
     );
 
-    String encryptedOut = logi.logAndGetStringFromBytes(
+    String encryptedOut = utils.logAndGetStringFromBytes(
       "MSG OUT raw out going payload",
-      msgOutgoingPlain.toList(),
+      msgOutgoingPlain,
     );
 
-    String cryptedMessage = theSC.encrypt(msgOutgoingPlain);
+    String cryptedMessage = theSC.encrypt(encryptedOut);
 
-    logi.logAndGetBytesFromString("MSG OUT raw crypted", cryptedMessage);
+    utils.logAndGetBytesFromString("MSG OUT raw crypted", cryptedMessage);
 
     String outMessage = outgoingIVvector + cryptedMessage;
-    Uint8List tmp = logi.logAndGetBytesFromString(
+    Uint8List tmp = utils.logAndGetBytesFromString(
       "MSG out [Outgoing IV] + [crypted message]",
       outMessage,
     );
+
+    logi.traceMe("==============================================");
+    logi.traceMe("== lja1 ");
+    logi.traceMe("==============================================");
+    logi.traceMe("MSG decrypt try ============================");
+    this.theSecureMessageTransferIncoming(tmp, theAesKeyUintList);
+    logi.traceMe("MSG decrypt try end ========================");
+
     return tmp;
   }
 
--- src/stcp/lib/src/stcpServer.dart
+++ src/stcp/lib/src/stcpServer.dart
@@ -1,13 +1,14 @@
 library stcp;
 
 import 'dart:io';
-import 'dart:convert';
+import 'dart:async';
 import 'dart:typed_data';
 import 'package:the_tcp_messaging/server.dart' as tcpServer;
 import 'package:paxlog/paxlog.dart' as logi;
 
 import 'package:pointycastle/export.dart';
 import 'package:pointycastle/pointycastle.dart';
+import 'package:utils/utils.dart' as utils;
 
 import './stcpCommon.dart';
 
@@ -16,49 +17,58 @@ class StcpServer {
   int port;
   Map<String, dynamic> theSockedMetadata = {};
   processMsgFunction theUnsecureMessageHandler;
-  ServerSocket? socket;
-  late tcpServer.TcpServer theUnsecureServer;
+  late ServerSocket socket;
+  // late tcpServer.TcpServer theUnsecureServer;
   late StcpCommon theCommon;
   late EllipticCodec theEC = EllipticCodec();
-
-  String getKeyStringFromSock(Socket theSock) {
-    return "${theSock.address.host}:${theSock.port}";
-  }
 
   void set_socket_meta(Socket theSock, dynamic theMeta) {
     String key = getKeyStringFromSock(theSock);
     theSockedMetadata[key] = theMeta;
+    //utils.dump_pretty_json_from_object("Socket Meta (SET)", theSockedMetadata);
   }
 
   Map<String, dynamic> get_socket_meta(Socket theSock) {
     String key = getKeyStringFromSock(theSock);
+    //utils.dump_pretty_json_from_object("Socket Meta (GET)", theSockedMetadata);
     return theSockedMetadata[key] ?? {};
   }
 
-  Uint8List? theSecureMessageTransferAES(
-      Socket theSock, Uint8List msgIncomingCrypted) {
-    logi.logAndGetStringFromBytes(
-        "[SERVER] Secure AES transfer incoming", msgIncomingCrypted);
-    Map<dynamic, dynamic> theSockMeta = get_socket_meta(theSock);
-    Uint8List theAESpresharedkey = theSockMeta["AESkey"];
-    String theAESpresharedkeyStr =
-        logi.logAndGetStringFromBytes("Secure AES", theAESpresharedkey);
+  Future<Uint8List?> theSecureMessageTransferAES(Socket theSock,
+      Uint8List msgIncomingCrypted, Uint8List? theAESDerivedKey) async {
+    logi.traceIntList("Secure message transfer AES input", msgIncomingCrypted);
+    logi.traceIntList("The AES key", theAESDerivedKey);
+    if (theAESDerivedKey == null) {
+      throw Exception(
+          "STCP Protocol init failure: AES secret is not set to metadata!");
+    }
+
     Uint8List decryptedMessage = theCommon.theSecureMessageTransferIncoming(
-        msgIncomingCrypted, theAESpresharedkeyStr);
-    logi.traceMe(
-        "[SERVER] incoming dec: ${decryptedMessage.length} // $decryptedMessage //");
+        msgIncomingCrypted, theAESDerivedKey!);
+
+    logi.traceIntList("[SERVER] AES message decrypted", decryptedMessage);
 
     // the unsecure call
-    Uint8List? unsecureResponse =
-        theUnsecureMessageHandler(theSock, decryptedMessage);
+    logi.traceMe("Undesure callback call ===================================");
 
-    logi.traceMe(
-        "[SERVER] outgoing unsec: ${unsecureResponse?.length} // $unsecureResponse //");
-    Uint8List encryptedOut = theCommon.theSecureMessageTransferOutgoing(
-        unsecureResponse!, theAESpresharedkeyStr);
-    logi.traceMe(
-        "[SERVER] outgoing   sec: ${encryptedOut.length} // $encryptedOut //");
-    return encryptedOut;
+    Uint8List? unsecureResponse =
+        await theUnsecureMessageHandler(theSock, decryptedMessage);
+
+    logi.traceIntList("[SERVER] UNSEC returned", unsecureResponse);
+    logi.traceMe("Undesure callback done ===================================");
+
+    logi.traceIntList("[SERVER] outgoing", unsecureResponse);
+
+    if (unsecureResponse != null) {
+      Uint8List encryptedOut = theCommon.theSecureMessageTransferOutgoing(
+          unsecureResponse!, theAESDerivedKey!);
+
+      logi.traceIntList("[SERVER] finally outgoing", encryptedOut);
+      return encryptedOut;
+    }
+
+    logi.traceMe("[SERVER] finally outgoing, no response: return null");
+    return null;
   }
 
   Uint8List decryptAESKey(Uint8List encryptedKey, RSAPrivateKey privateKey) {
@@ -67,38 +77,49 @@ class StcpServer {
     return decryptor.process(encryptedKey);
   }
 
-  void theKeyExchangeProcess(Socket theSock) {
+  void theKeyExchangeProcess(Socket theSock) async {
     logi.traceMe(
-        'Connection from ${theSock.address.host}:${theSock.port} at handshake...');
+        'Connection from ${theSock.remoteAddress.host}:${theSock.remotePort} at handshake...');
     Map<String, dynamic>? tmpMap = get_socket_meta(theSock);
     bool needHandshake = true;
     if (tmpMap.isNotEmpty) {
       if (tmpMap.containsKey("AESkey")) {
         String tmpKey = tmpMap["AESkey"];
         if (tmpKey.isNotEmpty) {
-          logi.traceMe("Already have done handshake...");
-          needHandshake = false;
+          logi.traceMe(
+              "Already have done handshake but doing anyway: // $tmpKey // ...");
         }
       }
     }
+
     logi.traceMe("[STCP] Sending public key? $needHandshake ..");
     if (needHandshake) {
-      logi.traceMe("[STCP] Sending public key .....");
       Uint8List publicKeyBytes = theEC.getPublicKeyAsBytes();
-      theSock.add(publicKeyBytes);
-      theSock.flush();
+      logi.traceIntList("[STCP] Sending public key", publicKeyBytes);
+      try {
+        logi.traceIntList("Sending public key", publicKeyBytes);
+        theSock.add(publicKeyBytes);
+        theSock.flush();
+      } catch (e) {
+        logi.traceMe("Error while sending public key: $e");
+      }
+      logi.traceIntList("[STCP] Sent public key", publicKeyBytes);
     }
   }
 
   Future<void> setTheAesKey(Socket theSocket, Uint8List theAesKeyGot) async {
-    String tmpString =
-        logi.logAndGetStringFromBytes("SetTheAes:", theAesKeyGot.toList());
-    logi.traceMe("[STCP] Got AES key: $tmpString");
-    logi.traceMe(
-        "[STCP] For ${theSocket.address.host}:${theSocket.port} the AES key is: $tmpString");
-    Map<String, dynamic> tmp = get_socket_meta(theSocket);
-    tmp["AESkey"] = theAesKeyGot;
-    set_socket_meta(theSocket, tmp);
+    Map<String, dynamic>? tmpMap = get_socket_meta(theSocket);
+    if (tmpMap.isNotEmpty) {
+      if (tmpMap.containsKey("AESkey")) {
+        Uint8List tmpKey = tmpMap["AESkey"];
+        if (tmpKey.isNotEmpty) {
+          logi.traceIntList("Found AES derived key", tmpKey);
+        }
+      }
+    }
+    tmpMap["AESkey"] = theAesKeyGot;
+    logi.traceIntList("Updated AES derived key", theAesKeyGot);
+    set_socket_meta(theSocket, tmpMap);
   }
 
   Uint8List thePublicKeyGot(Socket theSocket, Uint8List thePublicKey) {
@@ -106,48 +127,214 @@ class StcpServer {
     return theEC.computeSharedSecretAsBytes(gotPubKey);
   }
 
-  Uint8List? theSecureMessageTransfer(
-      Socket theSock, Uint8List msgIncomingCrypted) {
-    logi.logAndGetStringFromBytes(
-        "[SERVER] Secure transfer incoming", msgIncomingCrypted);
+  Future<Uint8List?> theSecureMessageTransfer(
+      Socket theSock, Uint8List msgIncomingCrypted) async {
+    Map<String, dynamic> theMap = get_socket_meta(theSock);
+    if (theMap == null) {
+      logi.traceMe("Got no meta for socket! ${theSock}");
+      return null;
+    }
+    Uint8List? theAESDerivedKey = theMap["AESkey"];
 
-    Uint8List? theList =
-        theSecureMessageTransferAES(theSock, msgIncomingCrypted);
+    logi.traceIntList("[SERVER] Derived AES key", theAESDerivedKey);
+    logi.traceIntList("[SERVER] Data got in from AES", msgIncomingCrypted);
 
-    logi.logAndGetStringFromBytes(
-        "[SERVER] Secure transfer Outgoing", theList!.toList());
+    Uint8List? theList = await theSecureMessageTransferAES(
+        theSock, msgIncomingCrypted, theAESDerivedKey);
+
+    logi.traceIntList("[SERVER] theSecureMessageTransferAES Returned", theList);
+
+    if (theList == null) {
+      logi.traceMe("[SERVER] Secure transfer Outgoing was null");
+    }
 
     return theList;
   }
 
-  StcpServer(
-      this.host, this.port, String theAesKey, this.theUnsecureMessageHandler) {
+  StcpServer(this.host, this.port, this.theUnsecureMessageHandler) {
     theCommon = StcpCommon();
 
-    /*  theCommon.theAesKey = theAesKey
-        .padRight(STCP_AES_KEY_SIZE_IN_BYTES, "0")
-        .substring(0, STCP_AES_KEY_SIZE_IN_BYTES);*/
-
-    theUnsecureServer = tcpServer.TcpServer(
-        host,
-        port,
-        theEC,
-        theSecureMessageTransfer,
-        theKeyExchangeProcess,
-        setTheAesKey,
-        thePublicKeyGot);
     /*
       read() -> SECURE(decrypt) -> INSECURE -> SECURE(encrypt) -> send()
     */
-    logi.traceMe("Initialized StcpServer");
+    logi.traceMe("Initialized StcpServer at $host:$port ...");
   }
 
   Future<void> start() async {
-    await theUnsecureServer.start();
+    await bind();
+    await for (var sck in socket!) {
+      _handleConnection(sck);
+    }
+    logi.traceMe('Server started.');
   }
 
   Future<void> stop() async {
-    theUnsecureServer.stop();
     logi.traceMe('Server closed.');
+  }
+
+  Future<void> bind() async {
+    socket = await ServerSocket.bind(host, port);
+    logi.traceMe('Bound to ${socket!.address.address}:$port');
+  }
+
+  Map<String, Uint8List> theSharedKeys = {};
+  String getKeyStringFromSock(Socket theSock) {
+    return "${theSock.remoteAddress.host}:${theSock.remotePort}";
+  }
+
+  Future<void> _handleConnection(Socket theSocket) async {
+    bool gotPublicKey = false;
+    Uint8List? myAesKey;
+    Uint8List? theSharedSecret;
+    String sharedKeysKey = getKeyStringFromSock(theSocket);
+    bool gotClientPublicKey = false;
+
+    if (theSharedKeys.containsKey(sharedKeysKey)) {
+      Uint8List? tmpUI = theSharedKeys[sharedKeysKey];
+      if (tmpUI != null) {
+        gotClientPublicKey = true;
+        myAesKey = tmpUI!;
+        logi.traceIntList("Got already a key for $sharedKeysKey", myAesKey);
+      }
+    }
+
+//    if (!gotClientPublicKey) {
+    logi.traceMe("[STCP] Connecting to $host:$port .....");
+
+    if (theEC == null) {
+      logi.traceMe("Elliptic curve is null!");
+      throw Exception("STCP Protocol init failure: Elliptic Curve is not set.");
+      return;
+    }
+
+    logi.traceMe("[STCP] =========================================");
+    logi.traceMe("[STCP] ==== Handshake process ......");
+    logi.traceMe("[STCP] =========================================");
+    // Listen for publickey
+    StreamSubscription<Uint8List>? subscription;
+    /*
+      * Serveri lähettää ekana oman public keyn, vastineeksi
+      * serverille pitää tulla clientin public key
+      * 
+      * Serveri laittaa yhteisen salaisuuden AES avaimeksi, HMAC SHA256-käsit-
+      * telyllä.
+      *
+      */
+    logi.traceMe("[STCP] Sending my public key....");
+    this.theKeyExchangeProcess(theSocket);
+
+    subscription = theSocket.listen((theDataIn) async {
+      bool okLen = theDataIn.length == 65;
+      logi.traceIntList("at top of listen, data got (key? $okLen)", theDataIn);
+      if ((!gotClientPublicKey) && okLen) {
+        logi.traceIntList(
+            "[STCP] Got public key possibly from peer: $okLen", theDataIn);
+        theSharedSecret = this.thePublicKeyGot(theSocket, theDataIn);
+        if (theSharedSecret != null) {
+          myAesKey = theEC!.deriveSharedKeyBasedAESKey(theSharedSecret!);
+          if (myAesKey != null) {
+            setTheAesKey(theSocket, myAesKey!);
+            logi.traceIntList("[STCP] Got public key from client", myAesKey);
+            gotClientPublicKey = true;
+          }
+        }
+
+        if (gotClientPublicKey == false) {
+          logi.traceMe("[STCP] Re-Sending my public key....");
+          this.theKeyExchangeProcess(theSocket);
+        }
+      } else {
+        // Got public key!
+        logi.traceMe("[STCP] ==== AES Traffic processing ......");
+        logi.traceIntList(
+            "[STCP] ==== AES theSharedSecret set", theSharedSecret);
+        logi.traceIntList("[STCP] ==== AES key set", myAesKey);
+        logi.traceMe("[STCP] =========================================");
+        logi.traceIntList("[STCP] The AES traffic in", theDataIn);
+        /*
+        * Lopuksi: Clientti hanskaa AES salattuna liikenteen välittämistä
+        * STCP-kerrokselle.
+        */
+
+        logi.traceMe("[STCP] Calling the message handler..");
+        Uint8List? rv =
+            await this.theSecureMessageTransfer(theSocket, theDataIn);
+        logi.traceIntList("[STCP] Called, returned", rv);
+
+        if (rv != null) {
+          logi.traceMe("[STCP] sending returned....");
+          send_raw(theSocket, rv);
+        }
+      }
+    });
+  }
+
+  Uint8List? getAESFromSocket(Socket theSock) {
+    String sharedKeysKey = this.getKeyStringFromSock(theSock);
+    Uint8List? tmpUI = theSharedKeys[sharedKeysKey];
+
+    logi.traceIntList("[$sharedKeysKey] the Shared key", tmpUI);
+    if (tmpUI != null) {
+      Uint8List? myAesKey = theEC!.deriveSharedKeyBasedAESKey(tmpUI!);
+      logi.traceIntList("[$sharedKeysKey] theAES key", myAesKey);
+      return myAesKey;
+    }
+    return null;
+  }
+
+  Future<Uint8List?> stcp_recv(Socket theSocket) async {
+    // await initialize_socket();
+    Uint8List? theAESKey = getAESFromSocket(theSocket);
+    logi.traceIntList("the AES day at send", theAESKey);
+
+    Uint8List? encryptedIn = await this.recv_raw(theSocket);
+
+    Uint8List? theBytesOut = await theCommon.theSecureMessageTransferIncoming(
+        encryptedIn!, theAESKey!);
+
+    logi.traceIntList("STCP/recv", theBytesOut);
+    return theBytesOut;
+  }
+
+  Future<void> stcp_send(Socket theSocket, Uint8List message) async {
+    // await initialize_socket();
+    Uint8List? theAESKey = getAESFromSocket(theSocket);
+    logi.traceIntList("STCP/send/the AES day at send", theAESKey);
+    logi.traceIntList("STCP/send/the message", message);
+
+    Uint8List? theBytesOut =
+        await theCommon.theSecureMessageTransferOutgoing(message, theAESKey!);
+    logi.traceIntList("STCP/send/to wire:", theBytesOut);
+    send_raw(theSocket, message);
+  }
+
+  Future<void> send_raw(Socket theSocket, Uint8List message) async {
+    // await initialize_socket();
+    logi.traceMe("Sending to $host:$port via socket $theSocket");
+
+    String HN = theSocket.remoteAddress.host;
+    int HP = theSocket.remotePort;
+
+    logi.traceIntList("TCP/send to $HN:$HP", message);
+
+    //Socket.connect(host, port).then((theSck) {
+    theSocket.add(message);
+    //  theSck.close();
+    //});
+  }
+
+  Future<Uint8List?> recv_raw(Socket theSocket) async {
+    // await initialize_socket();
+
+    String HN = theSocket.address.host;
+    int HP = theSocket.port;
+    Uint8List? message = null;
+    logi.traceMe("TCP/recv from target: $HN:$HP.....");
+    RawSocket rawSocket =
+        await RawSocket.connect(theSocket.address, theSocket.port);
+    message = await rawSocket.read(10240);
+    rawSocket.close();
+    logi.traceIntList("TCP/recv ${message?.length}", message);
+    return message;
   }
 }

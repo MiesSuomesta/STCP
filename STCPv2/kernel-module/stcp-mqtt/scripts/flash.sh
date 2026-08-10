76dfa422c33be26da03aef6f3b3c53711812d5c8
Lauri Jakku <lja@lja.fi>
2026-08-10 09:47:20 +0300

    Snapshot commit for support package creation.

--- STCPv2/kernel-module/stcp-mqtt/scripts/flash.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /home/pomo/git/STCP/STCPv2/zephyr/nordic/ncs-3.3.0 && (
-	west flash -d ../stcp-mqtt/build-nrf9151
-)
--- STCPv2/kernel-module/x86-kernel-module/testing/collect-results.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir -p results
-dmesg > results/dmesg.txt
-uname -a > results/uname.txt
-lsmod > results/lsmod.txt
-cp /proc/crypto results/proc_crypto.txt
-echo "Collected into results/"
